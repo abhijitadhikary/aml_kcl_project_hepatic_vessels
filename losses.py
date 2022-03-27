1a2f00ff0d396b839e8b9f1b8654c602329a555b
Abhijit Adhikary <abhi.drmc@gmail.com>
2022-03-27 22:33:54 +0100

    ce and gen dice working (updated)

---
+++ losses.py
@@ -0,0 +1,51 @@
+import torch
+
+def flatten(tensor):
+    """Flattens a given tensor such that the channel axis is first.
+    The shapes are transformed as follows:
+       (N, C, D, H, W) -> (C, N * D * H * W)
+       Adapted from https://github.com/wolny/pytorch-3dunet/blob/master/pytorch3dunet/unet3d/losses.py
+    """
+    # number of channels
+    C = tensor.size(1)
+    # new axis order
+    axis_order = (1, 0) + tuple(range(2, tensor.dim()))
+    # Transpose: (N, C, D, H, W) -> (C, N, D, H, W)
+    transposed = tensor.permute(axis_order)
+    # Flatten: (C, N, D, H, W) -> (C, N * D * H * W)
+    return transposed.contiguous().view(C, -1)
+
+class GeneralizedDiceLoss():
+    """Computes Generalized Dice Loss (GDL) as described in https://arxiv.org/pdf/1707.03237.pdf.
+    Adapted from https://github.com/wolny/pytorch-3dunet/blob/master/pytorch3dunet/unet3d/losses.py
+    """
+
+    def __init__(self, normalization='sigmoid', epsilon=1e-6):
+        # super().__init__(weight=None, normalization=normalization)
+        self.epsilon = epsilon
+
+    def dice(self, input, target):
+        assert input.size() == target.size(), "'input' and 'target' must have the same shape"
+
+        input = flatten(input)
+        target = flatten(target)
+        target = target.float()
+
+        if input.size(0) == 1:
+            # for GDL to make sense we need at least 2 channels (see https://arxiv.org/pdf/1707.03237.pdf)
+            # put foreground and background voxels in separate channels
+            input = torch.cat((input, 1 - input), dim=0)
+            target = torch.cat((target, 1 - target), dim=0)
+
+        # GDL weighting: the contribution of each label is corrected by the inverse of its volume
+        w_l = target.sum(-1)
+        w_l = 1 / (w_l * w_l).clamp(min=self.epsilon)
+        w_l.requires_grad = False
+
+        intersect = (input * target).sum(-1)
+        intersect = intersect * w_l
+
+        denominator = (input + target).sum(-1)
+        denominator = (denominator * w_l).clamp(min=self.epsilon)
+
+        return 2 * (intersect.sum() / denominator.sum())
--- main.py
+++ main.py
@@ -20,6 +20,8 @@ from dataloader_center import DatasetHepatic
 # from network_resnet_deepmedic import DeepMedic
 from network_deepmedic import DeepMedic
 
+from losses import GeneralizedDiceLoss
+
 dataset = DatasetHepatic(run_mode='train', label_percentage=-1, use_probabilistic=True)
 dataloader = DataLoader(dataset, batch_size=1, shuffle=False)
 
@@ -27,88 +29,7 @@ dataloader = DataLoader(dataset, batch_size=1, shuffle=False)
 #     batch = next(iter(dataloader))
 #     print(f'{i}\t{batch[0].shape}\t{batch[1].shape}\t{batch[2].shape}')
 
-def flatten(tensor):
-    """Flattens a given tensor such that the channel axis is first.
-    The shapes are transformed as follows:
-       (N, C, D, H, W) -> (C, N * D * H * W)
-    """
-    # number of channels
-    C = tensor.size(1)
-    # new axis order
-    axis_order = (1, 0) + tuple(range(2, tensor.dim()))
-    # Transpose: (N, C, D, H, W) -> (C, N, D, H, W)
-    transposed = tensor.permute(axis_order)
-    # Flatten: (C, N, D, H, W) -> (C, N * D * H * W)
-    return transposed.contiguous().view(C, -1)
 
-class _AbstractDiceLoss(nn.Module):
-    """
-    Base class for different implementations of Dice loss.
-    """
-
-    def __init__(self, weight=None, normalization='sigmoid'):
-        super(_AbstractDiceLoss, self).__init__()
-        self.register_buffer('weight', weight)
-        # The output from the network during training is assumed to be un-normalized probabilities and we would
-        # like to normalize the logits. Since Dice (or soft Dice in this case) is usually used for binary data,
-        # normalizing the channels with Sigmoid is the default choice even for multi-class segmentation problems.
-        # However if one would like to apply Softmax in order to get the proper probability distribution from the
-        # output, just specify `normalization=Softmax`
-        assert normalization in ['sigmoid', 'softmax', 'none']
-        if normalization == 'sigmoid':
-            self.normalization = nn.Sigmoid()
-        elif normalization == 'softmax':
-            self.normalization = nn.Softmax(dim=1)
-        else:
-            self.normalization = lambda x: x
-
-    def dice(self, input, target):
-        # actual Dice score computation; to be implemented by the subclass
-        raise NotImplementedError
-
-    def forward(self, input, target):
-        # get probabilities from logits
-        input = self.normalization(input)
-
-        # compute per channel Dice coefficient
-        per_channel_dice = self.dice(input, target)
-
-        # average Dice score across all channels/classes
-        return 1. - torch.mean(per_channel_dice)
-
-class GeneralizedDiceLoss(_AbstractDiceLoss):
-    """Computes Generalized Dice Loss (GDL) as described in https://arxiv.org/pdf/1707.03237.pdf.
-    """
-
-    def __init__(self, normalization='sigmoid', epsilon=1e-6):
-        super().__init__(weight=None, normalization=normalization)
-        self.epsilon = epsilon
-
-    def dice(self, input, target):
-        assert input.size() == target.size(), "'input' and 'target' must have the same shape"
-
-        input = flatten(input)
-        target = flatten(target)
-        target = target.float()
-
-        if input.size(0) == 1:
-            # for GDL to make sense we need at least 2 channels (see https://arxiv.org/pdf/1707.03237.pdf)
-            # put foreground and background voxels in separate channels
-            input = torch.cat((input, 1 - input), dim=0)
-            target = torch.cat((target, 1 - target), dim=0)
-
-        # GDL weighting: the contribution of each label is corrected by the inverse of its volume
-        w_l = target.sum(-1)
-        w_l = 1 / (w_l * w_l).clamp(min=self.epsilon)
-        w_l.requires_grad = False
-
-        intersect = (input * target).sum(-1)
-        intersect = intersect * w_l
-
-        denominator = (input + target).sum(-1)
-        denominator = (denominator * w_l).clamp(min=self.epsilon)
-
-        return 2 * (intersect.sum() / denominator.sum())
 
 learning_rate = 0.0001
 momentum = 0.9
@@ -128,20 +49,19 @@ for index_epoch in tqdm(range(num_epochs), leave=False):
     loss_list_mse = []
     for index_batch, batch in enumerate(dataloader):
         if run_mode == 'train':
+            # ----------------------------------------------------------------------------------------------------------
+            # ----------------------------------------------------------------------------------------------------------
+            # ----------------------------------------------------------------------------------------------------------
             model.train()
             optimizer.zero_grad()
             image_patch_normal, image_patch_low, label_patch_out_real = batch
             image_patch_normal, image_patch_low, label_patch_out_real = image_patch_normal.to(device), image_patch_low.to(device), label_patch_out_real.to(device)
 
-            label_patch_out_pred_logits = model.forward((image_patch_normal, image_patch_low))
-
-            label_patch_out_pred = label_patch_out_pred_logits
-            # print(label_patch_out_pred)
+            # forward pass
+            label_patch_out_pred = model.forward((image_patch_normal, image_patch_low))
 
             # cross-entropy loss
-            # loss = criterion_ce(label_patch_out_pred.float(), label_patch_out_real.squeeze(1))
-
-
+            loss = criterion_ce(label_patch_out_pred.float(), label_patch_out_real.squeeze(1))
 
 
             # convert label_patch_out_real to one hot
@@ -153,25 +73,24 @@ for index_epoch in tqdm(range(num_epochs), leave=False):
             # generalized dice loss
             loss = criterion_dice(label_patch_out_pred.float(), label_patch_out_real_one_hot)
 
+            # calculate gradients and update weights
             loss.backward()
             optimizer.step()
 
             loss_list_mse.append(loss.item())
-
             print(loss.item())
+            # ----------------------------------------------------------------------------------------------------------
+            # ----------------------------------------------------------------------------------------------------------
+            # ----------------------------------------------------------------------------------------------------------
+
+        else:
+            # ----------------------------------------------------------------------------------------------------------
+            # ----------------------------------------------------------------------------------------------------------
+            # ----------------------------------------------------------------------------------------------------------
+            pass
 
     loss_mse_epoch = sum(loss_list_mse) / len(loss_list_mse)
 
     duration = time.time() - time_start
     print(f'Epoch:\t[{index_epoch+1} / {num_epochs}]\t\tTime:\t{duration} s\t\tMSE:\t{loss_mse_epoch:.5f}')
-
-print('Done')
-
-
-# def generalized_dice_loss(im_real, im_pred):
-#     total_voxels = im_real.shape[0] * im_real.shape[1] * im_real.shape[2], im_real.shape[3]
-#
-#     weights = [1 / np.power(np.sum(np.where(im_real == index, 1, 0)), 2) for index in range(3)]
-
-
 
