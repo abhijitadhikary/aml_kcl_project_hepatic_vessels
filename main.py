53328b2df7f22c01e2cf38702a696cfd29db820a
Abhijit Adhikary <abhi.drmc@gmail.com>
2022-03-28 11:10:42 +0100

    done - inference, speed up data loading timem, intra sample sampling, mse implemented (not being used atm), structured training functions
    pending - loss function improvement, network architecture, save/load, early stopping, lr-decay,

--- main.py
+++ main.py
@@ -53,6 +53,7 @@ dataset = DatasetHepatic(
     dataset_variant='npy',
     batch_size_inner=batch_size_inner
 )
+
 dataloader = DataLoader(
     dataset,
     batch_size=batch_size,
@@ -65,90 +66,161 @@ dataloader = DataLoader(
 
 
 learning_rate = 0.0001
-momentum = 0.9
-device = torch.device('cuda:0') if torch.cuda.is_available() else torch.device('cpu')
-model = DeepMedic().to(device)
+
+
+
 
 # optimizer = optim.SGD(model.parameters(), lr=learning_rate, momentum=momentum)
 
-optimizer = optim.Adam(
-    model.parameters(),
-    lr=learning_rate,
-    betas=(0.9, 0.999),
-    amsgrad=True
-)
 
-criterion_mse = nn.MSELoss()
-# criterion_mse = get_mse_loss
-criterion_dice = GeneralizedDiceLoss().dice
-criterion_ce = nn.CrossEntropyLoss()
+
+
 num_epochs = 10
 
-for index_epoch in tqdm(range(num_epochs), leave=False):
-    time_start = time.time()
+def run_epoch(dataloader, model, optimizer, criterion_dice, criterion_mse, criterion_ce, device, run_mode):
+    loss_list_dice = []
+    loss_list_ce = []
     loss_list_mse = []
+
     for index_batch, batch in enumerate(dataloader):
         if run_mode == 'train':
-            # ----------------------------------------------------------------------------------------------------------
-            # ----------------------------------------------------------------------------------------------------------
-            # ----------------------------------------------------------------------------------------------------------
             model.train()
             optimizer.zero_grad()
-            image_patch_normal, image_patch_low, label_patch_out_real = batch
-            image_patch_normal, image_patch_low, label_patch_out_real = image_patch_normal.to(
-                device), image_patch_low.to(device), label_patch_out_real.to(device)
-            if batch_size_inner > 1:
-                image_patch_normal, image_patch_low, label_patch_out_real = image_patch_normal.squeeze(
-                    0), image_patch_low.squeeze(0), label_patch_out_real.squeeze(0)
-            # forward pass
-            label_patch_out_pred = model.forward((image_patch_normal, image_patch_low))
+        else:
+            run_mode.eval()
 
-            # cross-entropy loss
-            # loss = criterion_ce(label_patch_out_pred.float(), label_patch_out_real.squeeze(1).long())
+        image_patch_normal, image_patch_low, label_patch_out_real = batch
+        image_patch_normal, image_patch_low, label_patch_out_real = image_patch_normal.to(device), image_patch_low.to(device), label_patch_out_real.to(device)
+        if batch_size_inner > 1:
+            image_patch_normal, image_patch_low, label_patch_out_real = image_patch_normal.squeeze(0), image_patch_low.squeeze(0), label_patch_out_real.squeeze(0)
 
-            # convert label_patch_out_real to one hot
-            label_patch_out_real_one_hot = torch.zeros_like(label_patch_out_pred).to(device)
-            # label_patch_out_real_one_hot[:, 0] = torch.where(label_patch_out_real == 0, 0, 1)
-            # label_patch_out_real_one_hot[:, 1] = torch.where(label_patch_out_real == 1, 1, 1)
-            # label_patch_out_real_one_hot[:, 2] = torch.where(label_patch_out_real == 2, 2, 1)
+        # forward pass
+        label_patch_out_pred = model.forward((image_patch_normal, image_patch_low))
 
-            label_patch_out_real_one_hot[:, 0] = torch.where(label_patch_out_real == 0, 1, 0).squeeze(1)
-            label_patch_out_real_one_hot[:, 1] = torch.where(label_patch_out_real == 1, 1, 0).squeeze(1)
-            label_patch_out_real_one_hot[:, 2] = torch.where(label_patch_out_real == 2, 1, 0).squeeze(1)
+        # cross-entropy loss
+        # loss = criterion_ce(label_patch_out_pred.float(), label_patch_out_real.squeeze(1).long())
 
-            # generalized dice loss
-            loss = criterion_dice(label_patch_out_pred.float(), label_patch_out_real_one_hot)
+        # convert label_patch_out_real to one hot
+        label_patch_out_real_one_hot = torch.zeros_like(label_patch_out_pred).to(device)
 
+        label_patch_out_real_one_hot[:, 0] = torch.where(label_patch_out_real == 0, 1, 0).squeeze(1)
+        label_patch_out_real_one_hot[:, 1] = torch.where(label_patch_out_real == 1, 1, 0).squeeze(1)
+        label_patch_out_real_one_hot[:, 2] = torch.where(label_patch_out_real == 2, 1, 0).squeeze(1)
+
+        # generalized dice loss
+        loss_dice = criterion_dice(label_patch_out_pred.float(), label_patch_out_real_one_hot)
+        loss_mse = criterion_mse(label_patch_out_pred.float(), label_patch_out_real_one_hot.float())
+
+        if run_mode == 'train':
             # calculate gradients and update weights
-            loss.backward()
+            loss_dice.backward()
             optimizer.step()
 
-            loss_list_mse.append(loss.item())
-            print(loss.item())
-            # ----------------------------------------------------------------------------------------------------------
-            # ----------------------------------------------------------------------------------------------------------
-            # ----------------------------------------------------------------------------------------------------------
+        loss_list_dice.append(loss_dice.item())
+        loss_list_mse.append(loss_mse)
+        # print(loss_dice.item())
+        # print(loss_mse.item())
 
-        elif run_mode == 'inference':
-            # ----------------------------------------------------------------------------------------------------------
-            # ----------------------------------------------------------------------------------------------------------
-            # ----------------------------------------------------------------------------------------------------------
-            images, labels = batch
-            images, labels = images.to(device), labels.to(device)
+    loss_dice_epoch = sum(loss_list_dice) / len(loss_list_dice)
+    loss_mse_epoch = 0
+    loss_ce_epoch = 0
 
-            labels_pred, model, optimizer, criterion_dice, loss = stride_depth_and_inference(model=model,
-                                                                                             optimizer=optimizer,
-                                                                                             criterion_dice=criterion_dice,
-                                                                                             images_real=images,
-                                                                                             labels_real=labels,
-                                                                                             patch_size_normal=25,
-                                                                                             patch_size_low=19,
-                                                                                             patch_size_out=9,
-                                                                                             patch_low_factor=3)
+    return model, optimizer, loss_dice_epoch, loss_mse_epoch, loss_ce_epoch
 
-            # calculate loss
+def fit_model(model_name, optimizer_name, dataloader_train, dataloader_val, learning_rate, num_epochs):
 
-    loss_mse_epoch = sum(loss_list_mse) / len(loss_list_mse)
+    device = torch.device('cuda:0') if torch.cuda.is_available() else torch.device('cpu')
 
-    duration = time.time() - time_start
-    print(f'Epoch:\t[{index_epoch + 1} / {num_epochs}]\t\tTime:\t{duration} s\t\tMSE:\t{loss_mse_epoch:.5f}')
+    if model_name == 'deep_medic':
+        model = DeepMedic().to(device)
+
+    criterion_mse = nn.MSELoss()
+    criterion_dice = GeneralizedDiceLoss().dice
+    criterion_ce = nn.CrossEntropyLoss()
+
+
+    if optimizer_name == 'adam':
+        optimizer = optim.Adam(
+            model.parameters(),
+            lr=learning_rate,
+            betas=(0.9, 0.999),
+            amsgrad=True
+        )
+    elif optimizer_name == 'sgd_w_momentum':
+        optimizer = optim.SGD(
+            model.parameters(),
+            lr=learning_rate,
+            momentum=0.9
+        )
+
+    for index_epoch in tqdm(range(num_epochs), leave=False):
+        time_start = time.time()
+
+        loss_list_dice_train = []
+        loss_list_dice_val = []
+        loss_list_mse_train = []
+        loss_list_mse_val = []
+
+        # train
+        model, optimizer, loss_dice_epoch, loss_mse_epoch, loss_ce_epoch = run_epoch(
+            dataloader=dataloader_train,
+            model=model,
+            optimizer=optimizer,
+            criterion_dice=criterion_dice,
+            criterion_mse=criterion_mse,
+            criterion_ce=criterion_ce,
+            device=device,
+            run_mode='train'
+        )
+
+        loss_list_dice_train.append(loss_dice_epoch)
+        loss_list_mse_train.append(loss_mse_epoch)
+
+        # validation
+        with torch.no_grad():
+            _, _, loss_dice_epoch, loss_mse_epoch, loss_ce_epoch = run_epoch(
+                dataloader=dataloader_val,
+                model=model,
+                optimizer=optimizer,
+                criterion_dice=criterion_dice,
+                criterion_mse=criterion_mse,
+                criterion_ce=criterion_ce,
+                device=device,
+                run_mode='val'
+            )
+
+        loss_list_dice_val.append(loss_dice_epoch)
+        loss_list_mse_val.append(loss_mse_epoch)
+
+        duration = time.time() - time_start
+        print(f'Epoch:\t[{index_epoch + 1} / {num_epochs}]\t\tTime:\t{duration} s\t\tMSE:\t{loss_mse_epoch:.5f}')
+
+fit_model(
+    model_name='deep_medic',
+    optimizer_name='adam',
+    dataloader_train=dataloader,
+    dataloader_val=dataloader,
+    learning_rate=0.001,
+    num_epochs=10
+)
+
+
+if run_mode == 'inference':
+    for index_batch, batch in enumerate(dataloader):
+        images, labels = batch
+        images, labels = images.to(device), labels.to(device)
+
+        labels_pred, model, optimizer, criterion_dice, loss = stride_depth_and_inference(
+            model=model,
+            optimizer=optimizer,
+            criterion_dice=criterion_dice,
+            images_real=images,
+            labels_real=labels,
+            patch_size_normal=patch_size_normal,
+            patch_size_low=patch_size_low,
+            patch_size_out=patch_size_out,
+            patch_low_factor=patch_low_factor
+        )
+
+
+
