932c7930e23438a377c7e705f7f4e1e4d799a33d
Abhijit Adhikary <abhi.drmc@gmail.com>
2022-04-01 09:31:28 +0100

    early stopper implemented, need to check correctness
    pending - loss function improvement, network architecture, augmentation, uncertainty estimation, fix logger for resuming training, fix inference

--- main.py
+++ main.py
@@ -45,48 +45,12 @@ assert batch_size == 1 or batch_size_inner == 1, 'either batch_size or batch_siz
 
 train_percentage = 0.8
 
-
-# def save_model(args, save_condition, model, optimizer, scheduler, index_epoch, loss, loss_best):
-#     '''
-#         Saves the model, best and the latest
-#     '''
-#
-#     path_checkpoint = 'checkpoints'
-#     os.makedirs(path_checkpoint, exist_ok=True)
-#
-#     # save latest model at each epoch
-#     save_dict = {'index_epoch': index_epoch + 1,
-#                  'model_state_dict': model.state_dict(),
-#                  'optim_state_dict': optimizer.state_dict(),
-#                  'scheduler_state_dict': scheduler.state_dict(),
-#                  'args': args
-#                  }
-#
-#     save_path = os.path.join(path_checkpoint, f'{index_epoch + 1}.pth')
-#     torch.save(save_dict, save_path)
-#     save_path = os.path.join(path_checkpoint, f'latest.pth')
-#     torch.save(save_dict, save_path)
-#
-#     if loss < loss_best and save_condition:
-#         loss_best = loss
-#         save_dict = {'index_epoch': index_epoch + 1,
-#                      'model_state_dict': model.state_dict(),
-#                      'optim_state_dict': optimizer.state_dict(),
-#                      'args': args
-#                      }
-#
-#         save_path = os.path.join(path_checkpoint, f'best.pth')
-#         torch.save(save_dict, save_path)
-#         print(f'*********************** New best model saved at {index_epoch + 1} ***********************')
-#     return loss_best
-
-
 class ModelConainer():
 
     def __init__(self):
-        self.__init_model_params__()
+        self.__init_model_params()
 
-    def __init_model_params__(self):
+    def __init_model_params(self):
         self.params_model = {
             'model_name': 'deep_medic',
             'patch_size_normal': 25,
@@ -99,7 +63,7 @@ class ModelConainer():
             'init_timestamp': datetime.now().strftime("%H-%M-%S__%d-%m-%Y")
         }
 
-    def __init_train_params__(self):
+    def __init_train_params(self):
         self.run_mode = 'train'
         self.params_train = {
             'optimizer_name': 'adam',  # adam, sgd_w_momentum
@@ -173,7 +137,7 @@ class ModelConainer():
         assert self.params_train['batch_size'] == 1 or self.params_train[
             'batch_size_inner'] == 1, 'either must be 1, size issue'
 
-    def __init_inference_params__(self):
+    def __init_inference_params(self):
         self.run_mode = 'inference'
         self.params_inference = {
             'loss_name': 'dice',
@@ -188,14 +152,14 @@ class ModelConainer():
             'dirname_checkpoint': '',
         }
 
-    def __setup_logger__(self):
+    def __setup_logger(self):
 
         if not self.params_train['resume_condition']:
             reload(logging)
 
         logging.basicConfig(filename=self.params_train['path_logger_full'], encoding='utf-8', level=logging.DEBUG)
 
-    def __create_params_file__(self):
+    def __create_params_file(self):
         params_dict = {
             'params_model': self.params_model,
             'params_train': self.params_train
@@ -206,13 +170,13 @@ class ModelConainer():
         with open(self.params_train['path_params_full'], 'w') as outfile:
             outfile.write(params_dict)
 
-        self.__print__(f'{"*" * 100}')
-        self.__print__('\t\tTraining starting with params:')
-        self.__print__(f'{"*" * 100}')
-        self.__print__(f'{params_dict}')
-        self.__print__(f'{"*" * 100}')
+        self.__print(f'{"*" * 100}')
+        self.__print('\t\tTraining starting with params:')
+        self.__print(f'{"*" * 100}')
+        self.__print(f'{params_dict}')
+        self.__print(f'{"*" * 100}')
 
-    def __create_checkpoint_dir__(self):
+    def __create_checkpoint_dir(self):
         if self.params_train['resume_condition']:
             self.params_train['dirname_checkpoint'] = self.params_train['resume_dir'][:11]
             self.params_train['path_checkpoint_full'] = self.params_train['resume_dir']
@@ -234,14 +198,14 @@ class ModelConainer():
         os.makedirs(self.params_train['path_checkpoint_full'], exist_ok=True)
 
     def train(self):
-        self.__init_train_params__()
-        self.__create_checkpoint_dir__()
-        self.__create_params_file__()
-        self.__setup_logger__()
-        self.__fit_model__()
+        self.__init_train_params()
+        self.__create_checkpoint_dir()
+        self.__create_params_file()
+        self.__setup_logger()
+        self.__fit_model()
 
     def inference(self):
-        self.__init_inference_params__()
+        self.__init_inference_params()
         self.__run_inference()
 
     def __set_device(self):
@@ -317,7 +281,7 @@ class ModelConainer():
         self.criterion_dice = GeneralizedDiceLoss().dice
         self.criterion_ce = nn.CrossEntropyLoss()
 
-    def __define_optimizr__(self):
+    def __define_optimizr(self):
         if self.params_train['optimizer_name'] == 'adam':
             self.optimizer = optim.Adam(
                 self.model.parameters(),
@@ -335,7 +299,7 @@ class ModelConainer():
         else:
             raise NotImplementedError(f'Invalid choice of optimizer:\t{self.params_train["optimizer_name"]}')
 
-    def __define_lr_scheduler__(self):
+    def __define_lr_scheduler(self):
         if self.params_train['lr_scheduler_name'] == 'plateau':
             self.lr_scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(
                 self.optimizer,
@@ -343,12 +307,12 @@ class ModelConainer():
                 factor=self.params_train['factor_lr_scheduler'],
                 verbose=True)
 
-    def __put_to_device__(self, device, tensors):
+    def __put_to_device(self, device, tensors):
         for index, tensor in enumerate(tensors):
             tensors[index] = tensor.to(device)
         return tensors
 
-    def __get_one_hot_labels__(self, input, labels, squeeze_dim=None):
+    def __get_one_hot_labels(self, input, labels, squeeze_dim=None):
         output = torch.zeros((input.shape[0], len(labels), input.shape[2], input.shape[3], input.shape[4]),
                              dtype=input.dtype).to(input.device)
 
@@ -361,31 +325,31 @@ class ModelConainer():
 
         return output
 
-    def __early_stopper(self, loss_list_val, loss_least_val, early_stop_patience_counter, min_early_stop):
-        '''
-            Adapter from one of my previous implementations: https://github.com/abhijitadhikary/Twitter-Sentiment-Analysis-using-LSTM/blob/master/train.py
-        '''
-        valid_loss_list_cp = loss_list_val.copy()
-        valid_loss_list_cp = valid_loss_list_cp[::-1]
+    # def __early_stopper(self):
+    #     '''
+    #         Adapter from one of my previous implementations: https://github.com/abhijitadhikary/Twitter-Sentiment-Analysis-using-LSTM/blob/master/train.py
+    #     '''
+    #     valid_loss_list_cp = loss_list_val.copy()
+    #     valid_loss_list_cp = valid_loss_list_cp[::-1]
+    #
+    #     current_valid_loss = valid_loss_list_cp[0]
+    #     if current_valid_loss < loss_least_val:
+    #         loss_least_val = current_valid_loss
+    #         early_stop_patience_counter = 0
+    #
+    #     else:
+    #         early_stop_condition = True
+    #         for index in range(min_early_stop):
+    #             if current_valid_loss < valid_loss_list_cp[index]:
+    #                 early_stop_condition = False
+    #         if early_stop_condition is True:
+    #             early_stop_patience_counter += 1
+    #         else:
+    #             early_stop_patience_counter = 0
+    #
+    #     return loss_least_val, early_stop_patience_counter
 
-        current_valid_loss = valid_loss_list_cp[0]
-        if current_valid_loss < loss_least_val:
-            loss_least_val = current_valid_loss
-            early_stop_patience_counter = 0
-
-        else:
-            early_stop_condition = True
-            for index in range(min_early_stop):
-                if current_valid_loss < valid_loss_list_cp[index]:
-                    early_stop_condition = False
-            if early_stop_condition is True:
-                early_stop_patience_counter += 1
-            else:
-                early_stop_patience_counter = 0
-
-        return loss_least_val, early_stop_patience_counter
-
-    def __save_model__(self):
+    def __save_model(self):
         '''
             Saves the model, best and the latest
         '''
@@ -416,7 +380,7 @@ class ModelConainer():
             if self.found_best_loss_flag:
                 save_path = os.path.join(self.params_train['path_checkpoint_full'], f'best.pth')
                 torch.save(save_dict, save_path)
-                self.__print__(f'{"*" * 10}\tNew best model saved at:\t{self.index_epoch + 1}\t{"*" * 10}')
+                self.__print(f'{"*" * 10}\tNew best model saved at:\t{self.index_epoch + 1}\t{"*" * 10}')
 
     def __load_model(self):
         '''
@@ -445,10 +409,10 @@ class ModelConainer():
                 self.loss_best_train = checkpoint['loss_best_train']
                 self.loss_best_val = checkpoint['loss_best_val']
 
-                self.__print__(f'Model loaded from epoch:\t{self.index_epoch}')
+                self.__print(f'Model loaded from epoch:\t{self.index_epoch}')
                 self.index_epoch += 1
                 self.start_epoch = self.index_epoch
-                self.__print__(f'Resuming training from epoch:\t{self.index_epoch}')
+                self.__print(f'Resuming training from epoch:\t{self.index_epoch}')
 
         elif self.run_mode == 'inference':
             filename_checkpoint = f'{self.params_inference["resume_epoch"]}.pth'
@@ -464,18 +428,35 @@ class ModelConainer():
             self.model.load_state_dict(checkpoint['model_state_dict'])
             self.params_model = checkpoint['params_model']
             self.index_epoch += 1
-            self.__print__(f'Model loaded from epoch:\t{self.index_epoch}')
+            self.__print(f'Model loaded from epoch:\t{self.index_epoch}')
 
-    def __early_stop__(self):
+    def __early_stop(self):
         '''
             If early stopping condition meets, break training
         '''
-        # TODO fix this early stopper
+        # train for at least self.params_train['min_epochs_to_train'] epochs
         if self.index_epoch > self.params_train['min_epochs_to_train']:
-            if self.params_train['early_stop_patience_counter'] > self.params_train['patience_early_stop']:
-                self.__print__(f'Early stopping at epoch:\t{self.index_epoch + 1}')
 
-    def __run_epoch__(self, dataloader, run_mode):
+            # if the latest loss is lower than the best, continue training,
+            # otherwise check the last x losses
+            if self.loss_best_val['total'][-1] < min(self.loss_best_val['total']):
+                self.break_training_condition = False
+            else:
+                index_start = self.loss_best_val['total'] - 1
+                index_stop = self.loss_best_val['total'] - 1 - self.params_train['patience_early_stop']
+
+                # if any of the last x losses are greater than the best loss, increase counter
+                counter = 0
+                for index in range(index_start, index_stop, -1):
+                    if self.loss_best_val['total'][index] > min(self.loss_best_val['total']):
+                        counter += 1
+
+                # if counter equals the patience, break training
+                if counter >= self.params_train['patience_early_stop']:
+                    self.__print(f'Early stopping at epoch:\t{self.index_epoch + 1}')
+                    self.break_training_condition = True
+
+    def __run_epoch(self, dataloader, run_mode):
         '''
             Runs one epoch of training and validation loops
         '''
@@ -493,7 +474,7 @@ class ModelConainer():
             else:
                 self.model.eval()
 
-            (image_patch_normal, image_patch_low, label_patch_out_real) = self.__put_to_device__(self.device, batch)
+            (image_patch_normal, image_patch_low, label_patch_out_real) = self.__put_to_device(self.device, batch)
 
             if self.params_train['batch_size_inner'] > 1:
                 image_patch_normal, image_patch_low, label_patch_out_real = image_patch_normal.squeeze(
@@ -503,8 +484,8 @@ class ModelConainer():
             label_patch_out_pred = self.model.forward((image_patch_normal, image_patch_low))
 
             # convert label_patch_out_real to one hot
-            label_patch_out_real_one_hot = self.__get_one_hot_labels__(label_patch_out_real, labels=[0, 1, 2],
-                                                                       squeeze_dim=1)
+            label_patch_out_real_one_hot = self.__get_one_hot_labels(label_patch_out_real, labels=[0, 1, 2],
+                                                                     squeeze_dim=1)
 
             # generalized dice loss_total # dice, mse, dice_n_mse
             if self.params_train['loss_name'] == 'dice':
@@ -541,7 +522,7 @@ class ModelConainer():
                 loss_total.backward()
                 self.optimizer.step()
             sep = '\t' if run_mode == 'train' else '\t\t'
-            self.__print__(f'\tBatch:\t[{index_batch + 1} / {len(dataloader)}]'
+            self.__print(f'\tBatch:\t[{index_batch + 1} / {len(dataloader)}]'
                            f'\n\t\t{str(run_mode).upper()}{sep}-->\t\tLoss ({self.params_train["loss_name"]}):\t\t{loss_total.item():.5f}')
 
             logging.debug('Debugger working here')
@@ -587,7 +568,7 @@ class ModelConainer():
             self.loss_dict_val['ce'].append(loss_ce)
             self.loss_dict_val['dice_n_mse'].append(loss_mse)
 
-    def __update_best_losses__(self):
+    def __update_best_losses(self):
         '''
             Updates the best loss found so far
         '''
@@ -605,12 +586,12 @@ class ModelConainer():
                 if self.params_train['loss_name'] == key:
                     self.found_best_loss_flag = True
 
-    def __print__(self, message):
+    def __print(self, message):
         print(message)
         logging.debug(message)
         logging.debug('working')
 
-    def __fit_model__(self):
+    def __fit_model(self):
         '''
             Trains and validates a model given hyperparameters, model and optimizer name, dataloaders, num_epochs
         '''
@@ -620,8 +601,8 @@ class ModelConainer():
         self.__get_dataloaders('train')
         self.__define_model()
         self.__define_criterions()
-        self.__define_optimizr__()
-        self.__define_lr_scheduler__()
+        self.__define_optimizr()
+        self.__define_lr_scheduler()
 
         # variables to keep track of training progress
         self.start_epoch = 0
@@ -635,14 +616,14 @@ class ModelConainer():
             self.index_epoch = index_epoch
 
             # train
-            self.__run_epoch__(
+            self.__run_epoch(
                 dataloader=self.dataloader_train,
                 run_mode='train'
             )
 
             # validation
             with torch.no_grad():
-                self.__run_epoch__(
+                self.__run_epoch(
                     dataloader=self.dataloader_val,
                     run_mode='val'
                 )
@@ -652,16 +633,17 @@ class ModelConainer():
 
             duration = time.time() - time_start
 
-            self.__print__(f'\n{"-" * 100}'
+            self.__print(f'\n{"-" * 100}'
                            f'\nEpoch:\t[{index_epoch + 1} / {self.end_epoch}]\t\t'
                            f'Time:\t{duration:.2f} s'
                            f'\n\tTRAIN\t\t-->\t\tLoss Total:\t\t{self.loss_dict_train["total"][-1]:.5f}'
                            f'\n\tVAL\t\t\t-->\t\tLoss Total:\t\t{self.loss_dict_val["total"][-1]:.5f}'
                            f'\n{"-" * 100}\n')
 
-            self.__update_best_losses__()
-            self.__save_model__()
-            self.__early_stop__()
+            self.__update_best_losses()
+            self.__save_model()
+            self.__early_stop()
+            # self.__early_stopper()
 
             if self.break_training_condition:
                 break
@@ -676,22 +658,22 @@ class ModelConainer():
         self.__define_criterions()
         self.__load_model()
 
-        self.__print__(f'{"*" * 100}')
-        self.__print__('\t\tInference starting with params:')
-        self.__print__(f'{"*" * 100}')
-        self.__print__(f'params_model:\n{self.params_model}')
-        self.__print__(f'params_inference:\n{self.params_inference}')
-        self.__print__(f'{"*" * 100}')
+        self.__print(f'{"*" * 100}')
+        self.__print('\t\tInference starting with params:')
+        self.__print(f'{"*" * 100}')
+        self.__print(f'params_model:\n{self.params_model}')
+        self.__print(f'params_inference:\n{self.params_inference}')
+        self.__print(f'{"*" * 100}')
 
         for index_batch, batch in enumerate(self.dataloader_inference):
-            (images, labels) = self.__put_to_device__(self.device, batch)
+            (images, labels) = self.__put_to_device(self.device, batch)
 
             labels_pred, loss_dice, loss_mse = self.__stride_depth_and_inference(
                 images_real=images,
                 labels_real=labels
             )
 
-            self.__print__(f'{index_batch}\tLoss DICE:\t{loss_dice}\tLoss MSE:\t{loss_mse}')
+            self.__print(f'{index_batch}\tLoss DICE:\t{loss_dice}\tLoss MSE:\t{loss_mse}')
 
     def __stride_depth_and_inference(self, images_real, labels_real):
 
@@ -895,5 +877,5 @@ class ModelConainer():
 
 
 model_container = ModelConainer()
-# model_container.train()
-model_container.inference()
+model_container.train()
+# model_container.inference()
